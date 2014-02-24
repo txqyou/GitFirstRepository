654891b281205ffe47512da6633ef6421e807d9b
txqyou <txqyou@gmail.com>
2014-02-24 21:44:18 +0800

    thrid

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,8 @@
 public class helloWorld{
 	public static void main(String[] args){
+		int a = 1 ;
+		int b = 2 ;
+		System.out.println("a+b="+(a+b));
 		System.out.println("Hello World !");
 		System.out.println("Hi,git");
 		System.out.println("I'm coming second!");

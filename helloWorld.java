0310593af3030c02ff4b7aa6219131bcb4c0766c
txqyou <txqyou@gmail.com>
2014-02-24 21:38:48 +0800

    second

--- helloWorld.java
+++ helloWorld.java
@@ -2,5 +2,6 @@ public class helloWorld{
 	public static void main(String[] args){
 		System.out.println("Hello World !");
 		System.out.println("Hi,git");
+		System.out.println("I'm coming second!");
 	}
 }
